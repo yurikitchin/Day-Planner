242665907004df04c0e7b23bad8e3a9e3fb71afa
Yuri Kitchin <ykitchin@gmail.com>
2021-04-17 14:14:36 +0800

    add JS to change text area color

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -3,3 +3,17 @@ console.log(dateTimeDisplay.toString());
 console.log(dateTimeDisplay.format("dddd, MMMM Do YYYY, h:mm a"));
 //Populate current day p tage with day, date and time
 document.getElementById("currentDay").innerHTML = dateTimeDisplay.format("dddd, MMMM Do YYYY, h:mm a");
+var timeAMPM = dateTimeDisplay.format("h a")
+//Change colour of text area
+function textAreaColour(){
+    debugger
+    console.log(dateTimeDisplay.format("h a"))
+    console.log(document.getElementById(timeAMPM).children[0].textContent)
+    if (dateTimeDisplay.format("h a") == document.getElementById(timeAMPM).children[0].textContent){
+        document.getElementById("1pmArea").style.backgroundColor = "red"
+    }
+}
+textAreaColour()
+//if hour is > label, display textarea background as grey
+//if hour = label display textaea background as red
+//if hour <  label display textare background as green
