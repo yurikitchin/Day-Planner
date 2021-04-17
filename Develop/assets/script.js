3525376d3603a4510d569c42d39b261324ba8bba
Yuri Kitchin <ykitchin@gmail.com>
2021-04-17 20:15:17 +0800

    create function to turn past times grey

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -13,26 +13,28 @@ console.log(divTimeArray)
 
 //turns elements in container into an array
 
-
+var redIndex = ""
 
 function textAreaColour(){
     console.log(dateTimeDisplay.format("h a"))
     //need a for loop that cycles through each time block and sets the color
     for (let i = 0; i < divTimeArray.length; i++){
-        var changered = divTimeArray[i].id;
-        var arrayIndexNumber = divTimeArray.indexOf(divTimeArray[i]);   
-        console.log(changered)
+        var changeColor = divTimeArray[i].id;
+        var arrayIndexNumber = divTimeArray.indexOf(divTimeArray[i]);      
         console.log(arrayIndexNumber)
     //if hour = label display textaea background as red
     //dateTimeDisplay.format("h a") if statement code to be added back in
     if ("2 pm" == divTimeArray[i].id){
-        document.getElementById(changered).children[1].style.backgroundColor = "red"
-        changered = divTimeArray.indexOf(divTimeArray[i]);
-        // console.log("changered is =" + changered);
-        // console.log(divTimeArray.indexOf(divTimeArray[i]));
-    } else if (changered > divTimeArray.indexOf(divTimeArray[i])){
-        console.log("changered is =" + changered);
-        console.log(divTimeArray.indexOf(divTimeArray[i]));
+        document.getElementById(changeColor).children[1].style.backgroundColor = "red"
+        redIndex = divTimeArray.indexOf(divTimeArray[i]);
+        console.log(arrayIndexNumber)
+        
+    } else if  (redIndex === "") {
+        document.getElementById(changeColor).children[1].style.backgroundColor = "grey"
+    } else if (redIndex < arrayIndexNumber){
+        console.log("this works")
+    }
+    //else if (changered > divTimeArray.indexOf(divTimeArray[i])){
     }
     
     //if hour is > label, display textarea background as grey
@@ -43,9 +45,9 @@ function textAreaColour(){
     
  
     //if hour <  label display textare background as green
+} 
  
-    
- }}
+ 
 textAreaColour()
 
 
