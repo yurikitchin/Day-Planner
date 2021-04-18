87b7ee2b862cd92a239d601973f8972ca922ea01
Yuri Kitchin <ykitchin@gmail.com>
2021-04-18 10:25:01 +0800

    clean up code, add clear button js

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,7 +1,4 @@
 var dateTimeDisplay = moment();
-console.log(dateTimeDisplay.toString());
-console.log(dateTimeDisplay.format("dddd, MMMM Do YYYY, h:mm a"));
-
 //Populate current day p tage with day, date and time
 document.getElementById("currentDay").innerHTML = dateTimeDisplay.format("dddd, MMMM Do YYYY, h:mm a");
 var containerElement = document.querySelector(".container");
@@ -15,25 +12,23 @@ function textAreaColour(){
     //need a for loop that cycles through each time block and sets the color
     for (let i = 0; i < 11; i++){
         var changeColor = divTimeArray[i].id;
-        var arrayIndexNumber = divTimeArray.indexOf(divTimeArray[i]);          
+        var arrayIndexNumber = divTimeArray.indexOf(divTimeArray[i]);     
     //if its the start of the day set color as green
-    if (dateTimeDisplay.format("h") <= "6"){
+    if (dateTimeDisplay.format("h a") === "6 am"){
         document.getElementById(changeColor).children[1].style.backgroundColor = "#84F05B"
     //if hour = label display textaea background as red
     } else if (dateTimeDisplay.format("h a") == divTimeArray[i].id){
         document.getElementById(changeColor).children[1].style.backgroundColor = "red"
         redIndex = divTimeArray.indexOf(divTimeArray[i]);
-        console.log(arrayIndexNumber)
     //if hour is > label, display textarea background as grey    
     } else if  (redIndex === "") {
         document.getElementById(changeColor).children[1].style.backgroundColor = "grey"
     //if hour is < display as green
     } else if (redIndex < arrayIndexNumber){
-        console.log(changeColor)
         document.getElementById(changeColor).children[1].style.backgroundColor = "#84F05B"
     }}
     
-}  
+}
 textAreaColour()
 
 
@@ -53,45 +48,50 @@ var dayTasks = {
     area5pm: "",
 }
 
-//add click event to button, checks area button was clicked into and saves text from that area to corresponding dayTasks key
-var button = document.getElementsByClassName("btn")
-document.querySelectorAll('.btn').forEach(item => {
-    item.addEventListener('click', event =>{
-        //dayTasks = localStorage.getItem.JSON.parse('user')
-        console.log(dayTasks)
-        chooseDiv = event. currentTarget.parentElement
-        var textArea = chooseDiv.children[1]
-        var userInput = textArea.value
-        var textID = textArea.id
-        var existingStorage = JSON.parse(localStorage.getItem('user'))
-        dayTasks = existingStorage
-        console.log(dayTasks)
-        dayTasks[textID] = userInput
-        localStorage.setItem('user', JSON.stringify(dayTasks))
-        
+    //add click event to button, checks area button was clicked into and saves text from that area to corresponding dayTasks key
+    document.querySelectorAll('.areaPopulate').forEach(item => {
+        item.addEventListener('click', event =>{
+            //dayTasks = localStorage.getItem.JSON.parse('user')
+            console.log(dayTasks)
+            chooseDiv = event. currentTarget.parentElement
+            var textArea = chooseDiv.children[1]
+            var userInput = textArea.value
+            var textID = textArea.id
+            var existingStorage = JSON.parse(localStorage.getItem('user'))
+            dayTasks = existingStorage
+            console.log(dayTasks)
+            dayTasks[textID] = userInput
+            localStorage.setItem('user', JSON.stringify(dayTasks))
+            
+        })
     })
-})
 
-//function to populate each text area from local storge
-function populateText (){}
-   var populateObject = JSON.parse(localStorage.getItem('user'))
-   for (var key in populateObject){
-       if (populateObject.hasOwnProperty(key)){
-           var CheckKey = key
-           var populateKey = populateObject[key]
-    for (let i = 0; i < 11; i++){
-        selectIDtoPopulate = divTimeArray[i].children[1].id;
-       if (selectIDtoPopulate === CheckKey){
-        document.getElementById(selectIDtoPopulate).innerText = populateKey
-       }    
-   }} }
-populateText()
+ //function to populate each text area from local storge
+function populateText (){
+    var populateObject = JSON.parse(localStorage.getItem('user'))
+    for (var key in populateObject){
+        if (populateObject.hasOwnProperty(key)){
+            var CheckKey = key
+            var populateKey = populateObject[key]
+     for (let i = 0; i < 11; i++){
+         selectIDtoPopulate = divTimeArray[i].children[1].id;
+        if (selectIDtoPopulate === CheckKey){
+         document.getElementById(selectIDtoPopulate).innerText = populateKey
+        }    
+    }}}} 
+ 
+ populateText()
 
-//clear button changes all of the values in the dayTask object back to "" and saves to local storage
+ //clear button changes all of the values in the dayTask object back to "" and saves to local storage
 var clearBtn = document.getElementById('clear')
 //add event listener
 clearBtn.addEventListener ('click', clearFunction)
 //clear function also will reload window to leave all fields blank
 function clearFunction () {
-    console.log(dayTasks)
+    var existingStorage = JSON.parse(localStorage.getItem('user'))
+    existingStorage = dayTasks
+    localStorage.setItem('user', JSON.stringify(existingStorage))
+    location.reload()
 }
+   
+
